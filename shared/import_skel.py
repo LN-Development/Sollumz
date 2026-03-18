d934b2c5a7a2a229fc805c5abe12528d3886ced8
Luan <123782375+LN-Development@users.noreply.github.com>
2026-03-18 20:15:06 -0300

    Add initial bone import function in import_skel.py

---
+++ shared/import_skel.py
@@ -0,0 +1,10 @@
+import bpy
+import io
+
+def obj_import_bone_name(bone_name)
+    for auxBone, boneName in enumerate(boneAux):
+        
+        actualName = 
+        create_bpy(boneName, 
+        armature_obj,data)
+                boneAux[auxBone]=boneName
